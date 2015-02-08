c85487c4c1e30f8f1ef518692c73daa401836880
Ian Sinnott <ian@iansinnott.com>
2015-02-07 20:59:01 -0800

    Add reactice Menu selected state

--- client/menu-bar/MenuBar.js
+++ client/menu-bar/MenuBar.js
@@ -2,27 +2,35 @@ var React = require('react');
 
 var MenuBar = React.createClass({displayName: "MenuBar",
 
+  getClassNames: function(i) {
+    var selected = this.state.currentlySelected === i;
+    return React.addons.classSet({
+      'icon-button': true,
+      'selected': selected
+    });
+  },
+
   /**
    * The 'Deposit Item' menu is selected initially.
    */
   getInitialState: function() {
-    return { currentlySelected: 1 };
+    return { currentlySelected: '1' };
   },
 
   navigateTo: function() {
     // TODO...
   },
 
-  handleClick: function() {
-    console.log('handle that click!');
-    // TODO...
+  handleClick: function(e) {
+    console.log(e.currentTarget.id);
+    this.setState({ currentlySelected: e.currentTarget.id });
   },
 
   render: function() {
     return (
       React.createElement("ul", {id: "menu-bar", className: "justify"}, 
         React.createElement("li", null, 
-          React.createElement("a", {id: 1, className: "icon-button selected", onClick: this.handleClick}, 
+          React.createElement("a", {id: "1", className: this.getClassNames('1'), onClick: this.handleClick}, 
             React.createElement("img", {src: "/images/deposit-item-unselected@3x.png"}), 
             React.createElement("img", {src: "/images/deposit-item-selected@3x.png", 
                  className: "selected"}), 
@@ -30,21 +38,21 @@ var MenuBar = React.createClass({displayName: "MenuBar",
           )
         ), 
         React.createElement("li", null, 
-          React.createElement("a", {id: 2, className: "icon-button", onClick: this.handleClick}, 
+          React.createElement("a", {id: "2", className: this.getClassNames('2'), onClick: this.handleClick}, 
             React.createElement("img", {src: "/images/all-items-unselected@3x.png"}), 
             React.createElement("img", {src: "/images/all-items-selected@3x.png", className: "selected"}), 
             React.createElement("span", null, "My Places")
           )
         ), 
         React.createElement("li", null, 
-          React.createElement("a", {id: 3, className: "icon-button", onClick: this.handleClick}, 
+          React.createElement("a", {id: "3", className: this.getClassNames('3'), onClick: this.handleClick}, 
             React.createElement("img", {src: "/images/history-unselected@3x.png"}), 
             React.createElement("img", {src: "/images/history-selected@3x.png", className: "selected"}), 
             React.createElement("span", null, "All Items")
           )
         ), 
         React.createElement("li", null, 
-          React.createElement("a", {id: 4, className: "icon-button", onClick: this.handleClick}, 
+          React.createElement("a", {id: "4", className: this.getClassNames('4'), onClick: this.handleClick}, 
             React.createElement("img", {src: "/images/my-places-unselected@3x.png"}), 
             React.createElement("img", {src: "/images/my-places-selected@3x.png", className: "selected"}), 
             React.createElement("span", null, "History")
--- client/menu-bar/MenuBar.jsx
+++ client/menu-bar/MenuBar.jsx
@@ -2,27 +2,35 @@ var React = require('react');
 
 var MenuBar = React.createClass({
 
+  getClassNames: function(i) {
+    var selected = this.state.currentlySelected === i;
+    return React.addons.classSet({
+      'icon-button': true,
+      'selected': selected
+    });
+  },
+
   /**
    * The 'Deposit Item' menu is selected initially.
    */
   getInitialState: function() {
-    return { currentlySelected: 1 };
+    return { currentlySelected: '1' };
   },
 
   navigateTo: function() {
     // TODO...
   },
 
-  handleClick: function() {
-    console.log('handle that click!');
-    // TODO...
+  handleClick: function(e) {
+    console.log(e.currentTarget.id);
+    this.setState({ currentlySelected: e.currentTarget.id });
   },
 
   render: function() {
     return (
       <ul id='menu-bar' className='justify'>
         <li>
-          <a id={1} className='icon-button selected' onClick={this.handleClick}>
+          <a id='1' className={this.getClassNames('1')} onClick={this.handleClick}>
             <img src='/images/deposit-item-unselected@3x.png' />
             <img src='/images/deposit-item-selected@3x.png'
                  className='selected' />
@@ -30,21 +38,21 @@ var MenuBar = React.createClass({
           </a>
         </li>
         <li>
-          <a id={2} className='icon-button' onClick={this.handleClick}>
+          <a id='2' className={this.getClassNames('2')} onClick={this.handleClick}>
             <img src='/images/all-items-unselected@3x.png' />
             <img src='/images/all-items-selected@3x.png' className='selected' />
             <span>My Places</span>
           </a>
         </li>
         <li>
-          <a id={3} className='icon-button' onClick={this.handleClick}>
+          <a id='3' className={this.getClassNames('3')} onClick={this.handleClick}>
             <img src='/images/history-unselected@3x.png' />
             <img src='/images/history-selected@3x.png' className='selected' />
             <span>All Items</span>
           </a>
         </li>
         <li>
-          <a id={4} className='icon-button' onClick={this.handleClick}>
+          <a id='4' className={this.getClassNames('4')} onClick={this.handleClick}>
             <img src='/images/my-places-unselected@3x.png' />
             <img src='/images/my-places-selected@3x.png' className='selected' />
             <span>History</span>
