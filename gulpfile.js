eb88bc01ce67b4994313e54e84714db83b81622e
Ian Sinnott <ian@iansinnott.com>
2015-02-07 19:13:31 -0800

    Add livereload for stylus.

--- gulpfile.js
+++ gulpfile.js
@@ -4,24 +4,25 @@ var gulp = require('gulp'),
     // ## Style
     concat = require('gulp-concat'),
     stylus = require('gulp-stylus'),
-    swiss = require('kouto-swiss'),
+    swiss  = require('kouto-swiss'),
 
     // ## Bundle
     browserify = require('browserify'),
-    watchify = require('watchify'),
-    envify = require('envify/custom'),
+    watchify   = require('watchify'),
+    envify     = require('envify/custom'),
     bundleName = require('vinyl-source-stream'),
 
     // ## utils
     plumber = require('gulp-plumber'),
-    watch = require('gulp-watch'),
-    debug = require('debug')('sp:gulp'),
+    watch   = require('gulp-watch'),
+    debug   = require('debug')('sp:gulp'),
 
     // ## React
     react = require('gulp-react'),
 
     // serve
-    nodemon = require('gulp-nodemon');
+    nodemon = require('gulp-nodemon'),
+    lr      = require('gulp-livereload');
 
 var reloadDelay = 7000;
 
@@ -85,7 +86,12 @@ gulp.task('stylus', function() {
 });
 
 gulp.task('watch', function() {
+  lr.listen();
   gulp.watch(paths.stylusAll, ['stylus']);
+  gulp.watch(paths.css + '/main.css', function() {
+    console.log('hey you changed that file bro!');
+    lr.changed(paths.css + '/main.css');
+  });
 });
 
 gulp.task('jsx', function() {
