086a946ffc5282cc14cb6aeb893e1575d35488aa
Ian Sinnott <ian@iansinnott.com>
2015-02-08 16:47:41 -0800

    Add sort-bar animation and style

--- client/all-items/sort-menu/SortMenu.js
+++ client/all-items/sort-menu/SortMenu.js
@@ -9,70 +9,86 @@ var SortMenu = React.createClass({displayName: "SortMenu",
       'selected': selected
     });
   },
+
+  /**
+   * Slide down the menu.
+   */
+  slideToggle: function() {
+    this.setState({ isOpen: !this.state.isOpen });
+  },
   
   getInitialState: function() {
     return {
       currentlySelected: '1',
-      sortBy: 'date'
+      sortBy: 'date',
+      isOpen: false
     };
   },
 
-  handleClick: function(e) {
-
-    var sortBy = e.currentTarget.getAttribute('data-sortBy');
-
-    // Debug
-    var obj = {
-      'date': 'Recent',
-      'alphabetical': 'A-Z',
-      'favorites': 'Heart',
-      'locked': 'Padlock'
-    };
-
-    console.log('hey no sorting by %s!', obj[sortBy]);
+  /**
+   * When clicked the clicked link should get the selected class and tell the
+   * app to sort the current list of elements by the sortBy param.
+   *
+   * Note: This method is meant to be bound with the first argument set to a
+   * sortBy string. Otherwise this will be called as a standard event handler
+   * and the only arg passed will be the event obj.
+   */
+  handleClick: function(sortBy, e) {
+    var link   = e.currentTarget;
+    console.log('hey no sorting by %s!', sortBy); // Debug/Todo
+    this.setState({ currentlySelected: link.id });
   },
 
   render: function() {
+
+    // 'open' class determines whether the component is visible or not
+    var classes = React.addons.classSet({
+      'open': this.state.isOpen
+    });
+
     return (
-      React.createElement("div", {id: "sort-menu"}, 
+      React.createElement("div", {id: "sort-menu", className: classes}, 
         React.createElement("ul", {className: "horizontal-menu"}, 
           React.createElement("li", null, 
             React.createElement("a", {
               id: "1", 
-              "data-sortBy": "date", 
+              sortBy: "date", 
               className:  this.getClassNames('1'), 
-              onClick:  this.handleClick}, 
+              onClick:  this.handleClick.bind(this, 'date') }, 
               React.createElement("span", null, "Recent")
             )
           ), 
           React.createElement("li", null, 
             React.createElement("a", {
               id: "2", 
-              "data-sortBy": "alphabetical", 
+              sortBy: "alphabetical", 
               className:  this.getClassNames('2'), 
-              onClick:  this.handleClick}, 
+              onClick:  this.handleClick.bind(this, 'alphabetical') }, 
               React.createElement("span", null, "A-Z")
             )
           ), 
           React.createElement("li", null, 
             React.createElement("a", {
               id: "3", 
+              sortBy: "favorites", 
               className:  this.getClassNames('3'), 
-              onClick:  this.handleClick}, 
+              onClick:  this.handleClick.bind(this, 'favorites') }, 
               React.createElement("img", {src: "/images/sort-menu/heart-unselected.png"}), 
-              React.createElement("img", {src: "/images/sort-menu/heart-unselected.png", className: "selected"})
+              React.createElement("img", {src: "/images/sort-menu/heart-selected.png", className: "selected"})
             )
           ), 
           React.createElement("li", null, 
             React.createElement("a", {
               id: "4", 
+              sortBy: "locked", 
               className:  this.getClassNames('4'), 
-              onClick:  this.handleClick}, 
+              onClick:  this.handleClick.bind(this, 'locked') }, 
               React.createElement("img", {src: "/images/sort-menu/padlock-unselected.png"}), 
-              React.createElement("img", {src: "/images/sort-menu/heart-unselected.png", className: "selected"})
+              React.createElement("img", {src: "/images/sort-menu/padlock-selected.png", className: "selected"})
             )
           )
-        )
+        ), 
+        React.createElement("button", {className: "toggle", onClick:  this.slideToggle})
       )
     );
   }
--- client/all-items/sort-menu/SortMenu.jsx
+++ client/all-items/sort-menu/SortMenu.jsx
@@ -9,70 +9,86 @@ var SortMenu = React.createClass({
       'selected': selected
     });
   },
+
+  /**
+   * Slide down the menu.
+   */
+  slideToggle: function() {
+    this.setState({ isOpen: !this.state.isOpen });
+  },
   
   getInitialState: function() {
     return {
       currentlySelected: '1',
-      sortBy: 'date'
+      sortBy: 'date',
+      isOpen: false
     };
   },
 
-  handleClick: function(e) {
-
-    var sortBy = e.currentTarget.getAttribute('data-sortBy');
-
-    // Debug
-    var obj = {
-      'date': 'Recent',
-      'alphabetical': 'A-Z',
-      'favorites': 'Heart',
-      'locked': 'Padlock'
-    };
-
-    console.log('hey no sorting by %s!', obj[sortBy]);
+  /**
+   * When clicked the clicked link should get the selected class and tell the
+   * app to sort the current list of elements by the sortBy param.
+   *
+   * Note: This method is meant to be bound with the first argument set to a
+   * sortBy string. Otherwise this will be called as a standard event handler
+   * and the only arg passed will be the event obj.
+   */
+  handleClick: function(sortBy, e) {
+    var link   = e.currentTarget;
+    console.log('hey no sorting by %s!', sortBy); // Debug/Todo
+    this.setState({ currentlySelected: link.id });
   },
 
   render: function() {
+
+    // 'open' class determines whether the component is visible or not
+    var classes = React.addons.classSet({
+      'open': this.state.isOpen
+    });
+
     return (
-      <div id="sort-menu">
+      <div id="sort-menu" className={classes}>
         <ul className='horizontal-menu'>
           <li>
             <a
               id='1'
-              data-sortBy='date'
+              sortBy='date'
               className={ this.getClassNames('1') }
-              onClick={ this.handleClick }>
+              onClick={ this.handleClick.bind(this, 'date') }>
               <span>Recent</span>
             </a>
           </li>
           <li>
             <a
               id='2'
-              data-sortBy='alphabetical'
+              sortBy='alphabetical'
               className={ this.getClassNames('2') }
-              onClick={ this.handleClick }>
+              onClick={ this.handleClick.bind(this, 'alphabetical') }>
               <span>A-Z</span>
             </a>
           </li>
           <li>
             <a
               id='3'
+              sortBy='favorites'
               className={ this.getClassNames('3') }
-              onClick={ this.handleClick }>
+              onClick={ this.handleClick.bind(this, 'favorites') }>
               <img src='/images/sort-menu/heart-unselected.png' />
-              <img src='/images/sort-menu/heart-unselected.png' className='selected' />
+              <img src='/images/sort-menu/heart-selected.png' className='selected' />
             </a>
           </li>
           <li>
             <a
               id='4'
+              sortBy='locked'
               className={ this.getClassNames('4') }
-              onClick={ this.handleClick }>
+              onClick={ this.handleClick.bind(this, 'locked') }>
               <img src='/images/sort-menu/padlock-unselected.png' />
-              <img src='/images/sort-menu/heart-unselected.png' className='selected' />
+              <img src='/images/sort-menu/padlock-selected.png' className='selected' />
             </a>
           </li>
         </ul>
+        <button className="toggle" onClick={ this.slideToggle } />
       </div>
     );
   }
